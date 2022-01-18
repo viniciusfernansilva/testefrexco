a183aad479ea83cb4b49859f9881ee7a87a6380b
viniciusfernansilva <viniciusfernansilva@gmail.com>
2022-01-18 16:22:56 -0300

    Adicionado Django Rest Framework

--- frexco1/frexco1/urls.py
+++ frexco1/frexco1/urls.py
@@ -17,15 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from frexcoapi import views
 
+
 urlpatterns = [
     path('', views.index),
-    path('regioes/', views.get_region),
-    path('frutas/', views.get_fruit),
-    path('frutas_por_regiao/', views.get_fruit_region),
-    path('regioes_por_id/', views.get_region_id),
-    path('frutas_por_id/', views.get_fruit_id),
-    path('cadastra_fruta', views.post_fruit),
-    path('atualiza_fruta', views.put_fruit),
-    path('excluir_fruta', views.delete_fruit),
+    path('regions/', views.regions),
+    path('regions/<int:regionid>', views.region_detail),
+    path('fruits/', views.fruits),
+    path('fruits/<int:fruitid>', views.fruit_detail),
     path('admin/', admin.site.urls),
 ]
--- frexco1/frexcoapi/views.py
+++ frexco1/frexcoapi/views.py
@@ -1,24 +1,48 @@
-from unicodedata import name
-from django.shortcuts import render
 from frexcoapi import models
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from rest_framework.decorators import api_view
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
-@csrf_exempt 
-def get_region(request):                    
+
+@csrf_exempt
+@api_view(['GET'])
+def regions(request):
     try:
         if request.method == 'GET':
             regioes = models.Region.objects.all().values()
             regioes = list(regioes)
             return JsonResponse(regioes, safe=False, status=200)
-        
-        else:
-            return JsonResponse({'mensagem' : 'O método passado não é do tipo GET.'}, status=405)
+    except Exception as e:
+        print(e)
+        return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
+
+
+   
+@csrf_exempt
+@api_view(['GET'])
+def region_detail(request, regionid):
+    try:
+        if request.method == 'GET':
+            try:
+                region = models.Region.objects.filter(id=regionid).values()
+                frutas_regiao = models.Fruit.objects.filter(regionid=regionid).values()
+            except:
+                return JsonResponse({'mensagem' : 'O ID consultado não é permitido.'}, status=400)
+            try:
+                region = list(region)[0]
+                frutas_regiao = list(frutas_regiao)
+            except:
+                return JsonResponse({'mensagem' : 'Não existe uma região com esse ID.'}, status=404)
+            response = {
+                'region': region,
+                'fruits': frutas_regiao
+            }    
+            return JsonResponse(response, status=200)
     except Exception as e:
         print(e)
         return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
@@ -26,95 +50,14 @@ def get_region(request):
 
 
 @csrf_exempt
-def get_fruit(request):                     
+@api_view(['GET', 'POST'])
+def fruits(request):
     try:
         if request.method == 'GET':
             fruits = models.Fruit.objects.all().values()
             fruits = list(fruits)
             return JsonResponse(fruits, safe=False, status=200)
-
-        else:
-            return JsonResponse({'mensagem': 'O método passado não é do tipo GET.'}, status=405)
-    except Exception as e:
-        print(e)
-        return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
-
-
-
-
-@csrf_exempt     #PEGAR FRUTAS POR REGIÃO
-def get_fruit_region(request):
-    try:
-        if request.method == 'GET':
-            pega_id = request.GET.get('regionid', 0)  
-            try:
-                frutas_regiao = models.Fruit.objects.filter(regionid=pega_id).values()
-            except:
-                return JsonResponse({'mensagem' : 'O ID consultado não é permitido.'}, status=400)
-
-
-            frutas_regiao = list(frutas_regiao)
-            return JsonResponse(frutas_regiao, safe=False, status=200)
-
-        else:
-            return JsonResponse({'mensagem': 'O método passado não é do tipo GET.'}, status=405)
-    except Exception as e:
-        print(e)
-        return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
-
-
-
-@csrf_exempt    #PEGAR REGIÃO POR ID
-def get_region_id(request):
-    try:
-        if request.method == 'GET':
-            pega_regiao_id = request.GET.get('regionid', 0)
-            try:
-                regioes_id = models.Region.objects.filter(id=pega_regiao_id).values()
-            except:
-                return JsonResponse({'mensagem' : 'O ID consultado não é permitido.'}, status=400)
-            try:
-                regioes_id = list(regioes_id)[0]
-            except:
-                return JsonResponse({'mensagem' : 'Não existe uma região com esse ID.'}, status=404)
-            return JsonResponse(regioes_id, status=200)
-
-        else:
-            return JsonResponse({'mensagem': 'O método passado não é do tipo GET.'}, status=405)
-
-    except Exception as e:
-        print(e)
-        return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
-
-
-@csrf_exempt
-def get_fruit_id(request):
-    try:
-        if request.method == 'GET':
-            pega_fruit_id = request.GET.get('fruitid', 0)
-            try:
-                fruit_id = models.Fruit.objects.filter(id=pega_fruit_id).values()
-            except:
-                return JsonResponse({'mensagem' : 'O ID consultado não é permitido.'}, status=400)
-            try:
-                fruit_id = list(fruit_id)[0]
-            except:
-                return JsonResponse({'mensagem': 'Não existe uma fruta com esse ID.'}, status=404)
-            
-            return JsonResponse(fruit_id, safe=False, status=200)
-        else:
-            return JsonResponse({'mensagem': 'O método passado não é do tipo GET.'}, status=405)
-
-    except Exception as e:
-        print(e)
-        return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
-
-
-
-@csrf_exempt
-def post_fruit(request):
-    try:
-        if request.method == 'POST':
+        elif request.method == 'POST':
             nome_fruta = request.POST.get('namefruit')
             region_id = request.POST.get('regionid')
             try:            
@@ -129,51 +72,43 @@ def post_fruit(request):
             except:
                 return JsonResponse({'mensagem' : 'Erro ao salvar os dados, verifique os campos.'}, status=400)
             return JsonResponse({'mensagem' : 'Fruta salva com sucesso.', 'fruitid' : fruta.id}, status=201)
-        else:
-            return JsonResponse({'mensagem': 'O método passado não é do tipo POST.'}, status=405)
-    
     except Exception as e:
         print(e)
         return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
 
 
-
 @csrf_exempt
-def put_fruit(request):
+@api_view(['GET', 'PUT', 'DELETE'])
+def fruit_detail(request,  fruitid):
     try:
-        if request.method == 'PUT':
+        if request.method == 'GET':
+            try:
+                fruit = models.Fruit.objects.filter(id=fruitid).values()
+            except:
+                return JsonResponse({'mensagem' : 'O ID consultado não é permitido.'}, status=400)
+            try:
+                fruit = list(fruit)[0]
+            except:
+                return JsonResponse({'mensagem': 'Não existe uma fruta com esse ID.'}, status=404)
+            
+            return JsonResponse(fruit, safe=False, status=200)
+        elif request.method == 'PUT':
             json_data = json.loads(request.body)
-            id_fruta = json_data['fruitid']
             novo_nome = json_data['newname']
             try:
-                fruit = models.Fruit.objects.filter(id=id_fruta)[0]
+                fruit = models.Fruit.objects.filter(id=fruitid)[0]
             except:
                 return JsonResponse({'mensagem' : 'O ID da fruta não foi encontrado.'}, status=404)
             fruit.name = novo_nome
             fruit.save(force_update=True)
             return JsonResponse({'mensagem' : 'Fruta atualizada com sucesso.'}, status=201)
-        else:
-            return JsonResponse({'mensagem': 'O método passado não é do tipo PUT.'}, status=405)
-    except Exception as e:
-        print(e)
-        return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
-
-
-
-@csrf_exempt
-def delete_fruit(request):
-    try:
-        if request.method == 'DELETE':
-            json_data = json.loads(request.body)
-            id_fruta = json_data['fruitid']
+        elif request.method == 'DELETE':
             try:
-                fruit = models.Fruit.objects.filter(id=id_fruta)[0]
+                fruit = models.Fruit.objects.filter(id=fruitid)[0]
             except:
                 return JsonResponse({'mensagem' : 'O ID da fruta não foi encontrado.'}, status=404)
             fruit.delete()
             return JsonResponse({'mensagem' : 'Fruta excluída com sucesso.'})
-        else:
-                return JsonResponse({'mensagem': 'O método passado não é do tipo DELETE.'}, status=405)
     except Exception as e:
         print(e)
         return JsonResponse({'mensagem' : 'Ocorreu um erro inesperado.'}, status=500)
